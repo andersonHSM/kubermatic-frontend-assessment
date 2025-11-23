16d892febf868a03290d93e45d6c52a647345d53
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 16:19:57 -0300

    Add login state handling in `LoginPage`; disable login button during submission; improve login flow with status signals and error handling

--- apps/web/src/app/pages/auth/login-page/login-page.ts
+++ apps/web/src/app/pages/auth/login-page/login-page.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { Password } from 'primeng/password';
+import { tap } from 'rxjs';
 
 import { AuthService } from '../../../services/auth.service';
 
@@ -17,8 +18,29 @@ export class LoginPage {
 	private readonly authService = inject(AuthService);
 	protected username = '';
 	protected password = '';
+	protected isLoggingIn = signal(false);
 
 	protected login(username: string, password: string) {
-		return this.authService.login(username, password).subscribe();
+		if (this.isLoggingIn()) {
+			return;
+		}
+
+		this.isLoggingIn.set(true);
+
+		return this.authService
+			.login(username, password)
+			.pipe(
+				tap({
+					next: () => {
+						console.log('Login successful');
+						this.isLoggingIn.set(false);
+					},
+					error: () => {
+						console.log('Login failed');
+						this.isLoggingIn.set(false);
+					},
+				}),
+			)
+			.subscribe();
 	}
 }
