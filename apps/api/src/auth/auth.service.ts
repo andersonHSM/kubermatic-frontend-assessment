a4b7f0d0c4eef3593f83342584511cf0ca7f9953
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-22 21:34:27 -0300

    Integrate `JwtService` in `AuthService` for JWT token generation during login.

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/login-dto';
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly usersService: UsersService) {}
+	constructor(
+		private readonly usersService: UsersService,
+		private readonly jwtService: JwtService,
+	) {}
 
 	public async login({ email, password }: LoginDto) {
 		const currentUser = await this.usersService.findOne(email);
 
-		console.log(currentUser);
 		if (!currentUser || password !== currentUser.password) {
 			return new UnauthorizedException("User or password doesn't match");
 		}
+
+		return this.jwtService.signAsync({ email: currentUser.email });
 	}
 }
