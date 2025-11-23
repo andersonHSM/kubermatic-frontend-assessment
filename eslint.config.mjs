2b767a9a9c5285751eab752f8cb3c46173817a29
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 15:08:28 -0300

    Refactor ESLint configuration with improved `import` plugin rules and custom resolver settings; update dependency versions and add relevant packages in `package-lock.json`.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,84 @@
+import importPlugin from 'eslint-plugin-import';
 import baseConfig from './eslint.base.config.mjs';
 
 export default [
-  ...baseConfig,
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
-  },
+	...baseConfig,
+	importPlugin.flatConfigs.recommended,
+	{
+		ignores: [
+			'**/dist',
+			'**/node_modules',
+			'**/coverage',
+			'**/.nx',
+			'**/.output',
+			'**/.idea',
+			'**/*.html',
+			'**/output/**',
+		],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		settings: {
+			'import/resolver': {
+				typescript: {
+					alwaysTryTypes: true,
+					typescript: true,
+					node: true,
+				},
+			},
+		},
+		rules: {
+			'import/first': ['error'],
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin', // Node.js built-in modules
+						'external', // Third-party modules
+						'internal', // Project's internal modules
+						'parent', // Parent directories
+						'sibling', // Sibling directories
+						'index', // Index files
+						'object', // Object imports
+						'type', // Type imports
+					],
+					'newlines-between': 'always', // Requires a blank line between import groups
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+					named: {
+						enabled: true,
+					},
+				},
+			],
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					enforceBuildableLibDependency: true,
+					allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+					depConstraints: [
+						{
+							sourceTag: '*',
+							onlyDependOnLibsWithTags: ['*'],
+						},
+					],
+				},
+			],
+		},
+	},
+	{
+		files: [
+			'**/*.ts',
+			'**/*.tsx',
+			'**/*.cts',
+			'**/*.mts',
+			'**/*.js',
+			'**/*.jsx',
+			'**/*.cjs',
+			'**/*.mjs',
+		],
+		// Override or add rules here
+		rules: {},
+	},
 ];
