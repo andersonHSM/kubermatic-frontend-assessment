7ae004a0e61db11b53d99f04f32c82eb8123b7e6
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 13:08:31 -0300

    Integrate form controls into `LoginPage` with PrimeNG components; implement basic login logic; update `tsconfig.json` to disable strict property initialization.

--- apps/web/src/app/pages/auth/login-page/login-page.ts
+++ apps/web/src/app/pages/auth/login-page/login-page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { Password } from 'primeng/password';
 
 @Component({
 	selector: 'app-login-page',
-	imports: [],
+	imports: [InputTextModule, FormsModule, FloatLabel, Password, ButtonModule],
 	templateUrl: './login-page.html',
 	styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+	protected username = '';
+	protected password = '';
+
+	protected login(username: string, password: string) {
+		console.log({ username, password });
+	}
+}
