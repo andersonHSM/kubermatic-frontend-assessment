3c3f767f9e09317b85e22beae96434ba53a85ca7
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 17:23:17 -0300

    Protect `/projects` route with `authGuard` for authenticated access.

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Route } from '@angular/router';
 
+import { authGuard } from './guards/auth/auth-guard';
 import { LoginPage } from './pages/auth/login-page/login-page';
 import { ProjectsPage } from './pages/dash/projects/projects-page/projects-page';
 
 export const appRoutes: Route[] = [
+	{ path: 'projects', component: ProjectsPage, canActivate: [authGuard] },
 	{ path: 'auth', children: [{ path: 'login', component: LoginPage }] },
-	{ path: 'projects', component: ProjectsPage },
 	{ path: '**', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
