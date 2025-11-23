7cd035e11d44685ea2a7c6db764165ecb0661435
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-22 23:43:34 -0300

    Add `@ApiBearerAuth` decorator to `VersionController` and `RegionController` for Swagger authentication documentation.

--- src/region/region.controller.ts
+++ src/region/region.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { RegionService } from './region.service';
 
+@ApiBearerAuth()
 @Controller('region')
 export class RegionController {
 	constructor(private readonly regionService: RegionService) {}
