042a230ee8fd2cb08a9e3364abbea9f20a31888e
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 00:07:53 -0300

    Integrate `ProjectsModule` into `AppModule`.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../domains/auth/auth.module';
+import { ProjectsModule } from '../domains/projects/projects.module';
 import { RegionModule } from '../domains/region/region.module';
 import { UsersModule } from '../domains/users/users.module';
 import { VersionModule } from '../domains/version/version.module';
@@ -13,6 +14,7 @@ import { AppService } from './app.service';
 		RegionModule,
 		AuthModule,
 		UsersModule,
+		ProjectsModule,
 		ConfigModule.forRoot({ isGlobal: true }),
 	],
 	controllers: [AppController],
