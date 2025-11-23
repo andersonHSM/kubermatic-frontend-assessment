7907585ca7211cfc8e0b30b352ec2725b61a310d
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-23 12:10:10 -0300

    Add PrimeNG and theme support to `app.config` and update dependencies in `package.json` and `package-lock.json`.

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -4,11 +4,20 @@ import {
 	provideZoneChangeDetection,
 } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
+		provideAnimationsAsync(),
+		providePrimeNG({
+			theme: {
+				preset: Aura,
+			},
+		}),
 		provideClientHydration(withEventReplay()),
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({ eventCoalescing: true }),
