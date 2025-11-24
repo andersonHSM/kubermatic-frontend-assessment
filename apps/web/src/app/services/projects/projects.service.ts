cc19e25892448144dc3070edda699b6d63c649e1
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-24 15:13:04 -0300

    Add `listProjects` method to `ProjectsService` using `HttpClient` for project fetching

--- apps/web/src/app/services/projects/projects.service.ts
+++ apps/web/src/app/services/projects/projects.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+import { Project } from '@db/output/generated/prisma';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class ProjectsService {
-  
+	private readonly httpClient = inject(HttpClient);
+
+	public listProjects() {
+		return this.httpClient.get<Project[]>('projects');
+	}
 }
