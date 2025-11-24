37f41f4ade06e1910ec0dc15bcd7313661bff33c
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-24 12:19:09 -0300

    Update `ProjectsPage` layout with a menubar and added structure; improve `ProjectsListPage` with forms, buttons, grid layout, and basic project listing logic.

--- apps/web/src/app/pages/dash/projects/projects-list-page/projects-list-page.ts
+++ apps/web/src/app/pages/dash/projects/projects-list-page/projects-list-page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
 	selector: 'app-projects-list-page',
-	imports: [],
+	imports: [FormsModule, FloatLabel, Button, Card, InputText],
 	templateUrl: './projects-list-page.html',
 	styleUrl: './projects-list-page.css',
 })
-export class ProjectsListPage {}
+export class ProjectsListPage {
+	protected projects = [1, 2, 3];
+	protected value = '';
+
+	protected createProject() {
+		// TODO implement create project
+	}
+}
--- apps/web/src/app/pages/dash/projects/projects-page/projects-page.ts
+++ apps/web/src/app/pages/dash/projects/projects-page/projects-page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
 	selector: 'app-projects-page',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, Menubar],
 	templateUrl: './projects-page.html',
 	styleUrl: './projects-page.css',
 })
-export class ProjectsPage {}
+export class ProjectsPage {
+	protected items: MenuItem[] = [{ label: 'Projects', routerLink: '/projects' }];
+}
