7dabf5ebe7b9af8f23734e7e72d6530b55730617
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-24 11:02:11 -0300

    Add `ProjectsListPage` component with HTML, CSS, and unit test setup

---
+++ apps/web/src/app/pages/dash/projects/projects-list-page/projects-list-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-projects-list-page',
+	imports: [],
+	templateUrl: './projects-list-page.html',
+	styleUrl: './projects-list-page.css',
+})
+export class ProjectsListPage {}
