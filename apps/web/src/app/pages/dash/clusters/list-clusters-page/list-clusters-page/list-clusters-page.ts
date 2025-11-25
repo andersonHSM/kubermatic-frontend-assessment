8b340e83d5731284c8c091aaea367966c5eef8d4
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-25 16:28:30 -0300

    Switch `ListClustersPage` to table layout: replace card-based view with PrimeNG table and buttons for actions.

--- apps/web/src/app/pages/dash/clusters/list-clusters-page/list-clusters-page/list-clusters-page.ts
+++ apps/web/src/app/pages/dash/clusters/list-clusters-page/list-clusters-page/list-clusters-page.ts
@@ -1,15 +1,15 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Card } from 'primeng/card';
-import { Ripple } from 'primeng/ripple';
+import { Button } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { distinctUntilChanged, filter, map, switchMap } from 'rxjs';
 
 import { ClustersService } from '../../../../../services/clusters/clusters.service';
 
 @Component({
 	selector: 'app-list-clusters-page',
-	imports: [AsyncPipe, Card, Ripple],
+	imports: [AsyncPipe, TableModule, Button],
 
 	templateUrl: './list-clusters-page.html',
 	styleUrl: './list-clusters-page.css',
