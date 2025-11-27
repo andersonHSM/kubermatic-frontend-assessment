444559e5336f4e447b73dd0e4a0b92559fbbcb9b
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-11-27 09:45:48 -0300

    Increase test readability and formatting in `ClusterWizard`, `ProjectsService`, and `RegionService` specs: replace spaces with tabs for consistency.

--- apps/web/src/app/services/projects/projects.service.spec.ts
+++ apps/web/src/app/services/projects/projects.service.spec.ts
@@ -1,39 +1,39 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { ProjectsService } from './projects.service';
 
 describe('ProjectsService', () => {
-  let service: ProjectsService;
-  let httpMock: HttpTestingController;
+	let service: ProjectsService;
+	let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-    });
-    service = TestBed.inject(ProjectsService);
-    httpMock = TestBed.inject(HttpTestingController);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+		service = TestBed.inject(ProjectsService);
+		httpMock = TestBed.inject(HttpTestingController);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 
-  afterEach(() => {
-    httpMock.verify();
-  });
+	afterEach(() => {
+		httpMock.verify();
+	});
 
-  it('listProjects should GET without params when no search provided', () => {
-    service.listProjects().subscribe();
-    const req = httpMock.expectOne(r => r.method === 'GET' && r.url === 'projects');
-    expect(req.request.params.keys().length).toBe(0);
-    req.flush([]);
-  });
+	it('listProjects should GET without params when no search provided', () => {
+		service.listProjects().subscribe();
+		const req = httpMock.expectOne(r => r.method === 'GET' && r.url === 'projects');
+		expect(req.request.params.keys().length).toBe(0);
+		req.flush([]);
+	});
 
-  it('listProjects should include search param when provided', () => {
-    service.listProjects('abc').subscribe();
-    const req = httpMock.expectOne('projects?search=abc');
-    expect(req.request.method).toBe('GET');
-    req.flush([]);
-  });
+	it('listProjects should include search param when provided', () => {
+		service.listProjects('abc').subscribe();
+		const req = httpMock.expectOne('projects?search=abc');
+		expect(req.request.method).toBe('GET');
+		req.flush([]);
+	});
 });
--- apps/web/src/app/services/region/region.service.spec.ts
+++ apps/web/src/app/services/region/region.service.spec.ts
@@ -1,32 +1,32 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { RegionService } from './region.service';
 
 describe('RegionService', () => {
-  let service: RegionService;
-  let httpMock: HttpTestingController;
+	let service: RegionService;
+	let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-    });
-    service = TestBed.inject(RegionService);
-    httpMock = TestBed.inject(HttpTestingController);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+		service = TestBed.inject(RegionService);
+		httpMock = TestBed.inject(HttpTestingController);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 
-  afterEach(() => {
-    httpMock.verify();
-  });
+	afterEach(() => {
+		httpMock.verify();
+	});
 
-  it('findAll should GET regions', () => {
-    service.findAll().subscribe();
-    const req = httpMock.expectOne('region');
-    expect(req.request.method).toBe('GET');
-    req.flush([]);
-  });
+	it('findAll should GET regions', () => {
+		service.findAll().subscribe();
+		const req = httpMock.expectOne('region');
+		expect(req.request.method).toBe('GET');
+		req.flush([]);
+	});
 });
